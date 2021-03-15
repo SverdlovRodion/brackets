f8221474c32a127c4dc83d8395d3bf881e8891b2
SverdlovRodion <78374115+SverdlovRodion@users.noreply.github.com>
2021-03-15 23:36:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = [];
+    let exist = {};
+    for(let j in bracketsConfig) {
+        exist[bracketsConfig[j][0]] = bracketsConfig[j][1];
+    }
+
+    res.push(str.charAt(0));
+
+    for(let i = 1; i < str.length; i += 1) {
+        if(str.charAt(i) === exist[res[res.length - 1]]) {
+            res.pop();
+        }
+        else
+        {
+            res.push(str.charAt(i));
+        }
+    }
+    if (res.length > 0) return false;
+    else return true;
 }
